05255bd3eff80b66d7e6416eb32e6a4a45387e21
Alaul1988 Sunan <alaul1988sunan@gmail.com>
2023-11-16 01:06:24 -0500

    first commit

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import profile from '../../assets/images/profile.png'
+
+const Header = () => {
+    return (
+        <div className='flex justify-between items-center p-4 border-b-2 max-w-7xl mx-auto'>
+             <h1 className='text-4xl font-bold'>React My Cafe</h1>
+             <img src={profile}/>
+            
+        </div>
+    );
+};
+
+export default Header;
